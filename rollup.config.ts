37733b0b36f2755f40e5f6ba972c815a4150a637
eleviven <firuz.haciyeff@gmail.com>
2024-02-11 00:04:26 +0400

    Modified rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,35 +1,43 @@
-import babel from "@rollup/plugin-babel";
-import external from "rollup-plugin-peer-deps-external";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import css from "rollup-plugin-import-css";
-import terser from "@rollup/plugin-terser";
+import packageJson from './package.json';
+
+import babel from '@rollup/plugin-babel';
+import external from 'rollup-plugin-peer-deps-external';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import css from 'rollup-plugin-import-css';
+import terser from '@rollup/plugin-terser';
+
+const name = packageJson.main.replace(/\.js$/, '');
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
     {
-      file: "dist/index.js",
-      format: "cjs",
-      exports: "auto",
+      file: `${name}.js`,
+      format: 'cjs',
+      exports: 'auto',
+      preserveModules: false,
+      sourcemap: true,
     },
     {
-      file: "dist/index.es.js",
-      format: "es",
-      exports: "auto",
+      file: `${name}.es.js`,
+      format: 'es',
+      exports: 'auto',
+      preserveModules: false,
+      sourcemap: true,
     },
   ],
   plugins: [
     external(),
     typescript({ clean: true }),
     babel({
-      configFile: "./.babelrc",
-      babelHelpers: "bundled",
+      configFile: './.babelrc',
+      babelHelpers: 'bundled',
     }),
     resolve(),
     commonjs(),
-    css({ output: "butalert.css" }),
+    css({ output: 'butalert.css' }),
     terser(),
   ],
 };
