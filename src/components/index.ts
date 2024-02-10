6cb51cc6e6fd25f697af46a3e8d5b8a9a6f44309
eleviven <firuz.haciyeff@gmail.com>
2024-02-10 19:59:05 +0400

    Exported spinner from index of components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button/Button";
+export { default as Spinner } from "./Spinner/Spinner";
 export { default as Popup } from "./Popup/Popup";
 export { default as PopupHeader } from "./Popup/PopupHeader";
 export { default as PopupBody } from "./Popup/PopupBody";
